6915435350045cb19351831159e818bd0c78600b
重生之我在崖州职业学院当保安 <17685537369@163.com>
2026-09-19 00:18:49 +0800

    refactor: rebuild homepage around immersive hero and feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import ProfileSidebar from "@/components/ProfileSidebar";
+import Hero from "@/components/Hero";
 import ProjectList from "@/components/ProjectList";
 
 export default function Home() {
   return (
-    <div
-      id="home"
-      className="mx-auto grid w-full max-w-[1120px] gap-10 px-5 pb-20 pt-24 lg:grid-cols-[238px_minmax(0,750px)] lg:justify-center lg:gap-[74px] lg:pt-[104px]"
-    >
-      <ProfileSidebar />
-      <main className="min-w-0">
+    <>
+      <Hero />
+      <main>
         <ProjectList />
       </main>
-    </div>
+    </>
   );
 }

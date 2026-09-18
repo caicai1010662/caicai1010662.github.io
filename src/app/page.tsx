e2f2793a4c643f05a6a3eaac54799f483255fd0e
重生之我在崖州职业学院当保安 <17685537369@163.com>
2026-09-18 23:53:32 +0800

    refactor: rebuild homepage as codermax-style project feed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 import ProfileSidebar from "@/components/ProfileSidebar";
 import ProjectList from "@/components/ProjectList";
-import Capabilities from "@/components/Capabilities";
-import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <div
       id="home"
-      className="mx-auto grid max-w-6xl gap-10 px-4 pb-8 pt-28 lg:grid-cols-[320px_minmax(0,1fr)] lg:gap-12"
+      className="mx-auto grid w-full max-w-[1180px] gap-12 px-5 pb-20 pt-24 lg:grid-cols-[280px_minmax(0,760px)] lg:justify-center lg:gap-16 lg:pt-28"
     >
       <ProfileSidebar />
-
       <main className="min-w-0">
         <ProjectList />
-        <Capabilities />
-        <Contact />
       </main>
     </div>
   );

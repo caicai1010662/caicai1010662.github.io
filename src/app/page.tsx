cbbc30b17730cfb562356c1b7bc3d79ef5884fdc
重生之我在崖州职业学院当保安 <17685537369@163.com>
2026-09-19 00:13:12 +0800

    style: rebalance homepage columns and first screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div
       id="home"
-      className="mx-auto grid w-full max-w-[1180px] gap-12 px-5 pb-20 pt-24 lg:grid-cols-[280px_minmax(0,760px)] lg:justify-center lg:gap-16 lg:pt-28"
+      className="mx-auto grid w-full max-w-[1120px] gap-10 px-5 pb-20 pt-24 lg:grid-cols-[238px_minmax(0,750px)] lg:justify-center lg:gap-[74px] lg:pt-[104px]"
     >
       <ProfileSidebar />
       <main className="min-w-0">
